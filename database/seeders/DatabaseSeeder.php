4cc37342933a4f4e1fa8cc4f7fc166b08d745a77
wonggadog <aldiangeldioquino.wong@bicol-u.edu.ph>
2025-03-19 16:14:30 +0800

    Added Menard to the Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,32 @@ class DatabaseSeeder extends Seeder
         //DB::table('users')->truncate();
 
         User::updateOrCreate(
-            ['email' => 'aldiangeldioquino.wong@bicol-u.edu.ph'], // Prevent duplicate entry
+            // First argument: Attributes to search for
+            ['email' => 'aldiangeldioquino.wong@bicol-u.edu.ph'],
+            // Second argument: Attributes to update or create
             [
                 'name' => 'Al Dian Gel Wong',
+                'email' => 'aldiangeldioquino.wong@bicol-u.edu.ph',
                 'email_verified_at' => now(),
-                // original code----- 'password' => bcrypt('password'),
                 'id_number' => '2021-2401-23944',
-                'password' => 'BicolUni2021',
+                'password' => bcrypt('BicolUni2021'), // Always hash passwords
+                'department' => 'Admin',
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+
+        User::updateOrCreate(
+            // First argument: Attributes to search for
+            ['email' => 'menardmonreal.miraballes@bicol-u.edu.ph'],
+            // Second argument: Attributes to update or create
+            [
+                'name' => 'Menard Miraballes',
+                'email' => 'menardmonreal.miraballes@bicol-u.edu.ph',
+                'email_verified_at' => now(),
+                'id_number' => '2021-2882-88633',
+                'password' => bcrypt('BicolUni2021'), // Always hash passwords
                 'department' => 'Admin',
                 'remember_token' => Str::random(10),
                 'created_at' => now(),
